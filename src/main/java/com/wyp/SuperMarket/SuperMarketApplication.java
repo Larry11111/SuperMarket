de886094e6586a93e5408590175691f8b71b5696
Larry11111 <916616442@qq.com>
2021-08-02 14:47:33 +0800

    dev新增

--- src/main/java/com/wyp/SuperMarket/SuperMarketApplication.java
+++ src/main/java/com/wyp/SuperMarket/SuperMarketApplication.java
@@ -1,8 +1,13 @@
 package com.wyp.SuperMarket;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class SuperMarketApplication {
+    public static void main(String[] args)  {
+        SpringApplication.run(SuperMarketApplication.class, args);
+    }
 }
 
